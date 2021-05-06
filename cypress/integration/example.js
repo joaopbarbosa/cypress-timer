6ce28035198a6a863d5943e1a10ae53c6a9a5de8
João Barbosa <up200803634@fc.up.pt>
2021-05-06 22:15:45 +0100

    Add seconds and minutes option

--- cypress/integration/example.js
+++ cypress/integration/example.js
@@ -5,7 +5,7 @@
 
 describe('Example to count time using Cypress.io', () => {
 
-    it('Search on Docs about cy.wait command', () => {
+    it('Simple count time (example in seconds)', () => {
         // Random 5 seconds before we start counting
         cy.wait(5000)
 
@@ -16,30 +16,31 @@ describe('Example to count time using Cypress.io', () => {
         cy.visit('https://docs.cypress.io/')
         cy.contains('Search').type('Wait')
         cy.contains('cy.wait').click({force:true})
-        cy.url().should('include', '/commands/waiting#wait')
 
         // Lets add + 5s just to test it
         cy.wait(5000) 
 
-        // Stop the time counting
-        cy.endcount() 
+        // Stop the time counting in seconds
+        cy.endcount('seconds') 
     })
 
-    it('Start counting when click on a button', () => {
+    it('Count time when do something (example in minutes)', () => {
         // Random 2 seconds before we start counting
         cy.wait(2000)
 
         // Click to start counting time
         cy.contains('Commands').click().startcount()
         
-        // Lets add + 3s just to test it
-        cy.wait(3000)
+        // Lets add + 60s just to test it
+        cy.wait(60000)
 
         // Test step
         cy.contains('Navigation').click()
 
-        // Focus and stop time counting
-        cy.contains('cy.visit').focus().endcount()
+        cy.wait(10000)
+
+        // Focus and stop time counting in minutes
+        cy.contains('cy.visit').focus().endcount('minutes')
     })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,16 +2,27 @@
 * Those are some commands to start and end our counter.
 */
 
-let endPoint, startPoint, timeSpended = null
+let endPoint, startPoint = null
+let timeSpendedSeconds, timeSpendedMinutes, timeSpendedHours  = null
 
 // Command to start counting time
 Cypress.Commands.add('startcount', () => {
     startPoint = new Date()
+    cy.log('_Start counting time..._')
 })
 
 // Command to stop our time counter
-Cypress.Commands.add('endcount', () => {
+Cypress.Commands.add('endcount', (timeFormat) => {
     endPoint = new Date()
-    timeSpended = (endPoint.getTime() - startPoint.getTime()) / 1000
-    cy.log(`Time for this test: ${timeSpended} seconds`)
+
+    if (timeFormat == 'seconds'){
+        timeSpendedSeconds = ((endPoint.getTime() - startPoint.getTime()) / 1000)
+        cy.log(`_Time for this test:_ **${timeSpendedSeconds.toFixed(2)} seconds**`)
+    }
+
+    else if (timeFormat == 'minutes') {    
+        timeSpendedMinutes = (((endPoint.getTime() - startPoint.getTime()) / 1000 ) / 60)
+        cy.log(`_Time for this test:_ **${timeSpendedMinutes.toFixed(2)} minutes**`)
+    } 
+
 })
